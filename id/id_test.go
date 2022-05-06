a6203d8c131ec0711b2ed50ee693d4fd72590117
SuperMekabu <yuki.military@gmail.com>
2022-05-07 01:34:19 +0900

    aaaaaaaaaaaaaaaa

--- id/id_test.go
+++ id/id_test.go
@@ -5,13 +5,12 @@ import (
 )
 
 func Test_objHash(t *testing.T) {
-
 	obj := []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	obj2 := []byte("!\"#$%&'()~=~|'")
 	hash1 := objHash(obj)
 	hash2 := objHash(obj)
 	hash3 := objHash(obj2)
-	t.Run("Same object hash", func(t *testing.T) {
+	t.Run("object hash", func(t *testing.T) {
 		if hash1 != hash2 {
 			t.Errorf("hash1 %v, hash2 %v", hash1, hash2)
 		}
